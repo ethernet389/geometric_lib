96d789dc0b257e238178562105557c216fb44d46
Gleb Simakov <ethernet389@gmail.com>
2024-11-04 22:12:01 +0300

    refactor(test.py): Add same values test for rectangle

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ class RectangleTestCase(unittest.TestCase):
         tests = [test1, test2, test3, test4]
         for test in tests:
             for a, b in iter.permutations(test, r=2):
-                self.assertEqual(function(*a), function(*b), msg)
+                self.assertEqual(function(*a), function(*b), f'{msg} : {a} | {b}')
 
     def test_area_commutativity(self):
         self.__commutativity_test(rectangle.area, "Area not commutative!")
@@ -23,3 +23,28 @@ class RectangleTestCase(unittest.TestCase):
 
     def test_perimeter_commutativity(self):
         self.__commutativity_test(rectangle.perimeter, "Perimeter not commutative!")
+
+    def __same_values_test(self, tests, function, msg=None):
+        for test in tests:
+            args = (test[0], test[0])
+            self.assertEqual(function(*args), test[1], f'{msg} : {args}')
+        
+    def test_area_same_values(self):
+        tests = [(0, 0),
+                 (1, 1),
+                 (5, 25),
+                 (10**100, 10**200),
+                 (10**1000, 10**2000),
+                 (1.2, 1.44),
+                 (0.01, 0.0001)]
+        self.__same_values_test(tests, rectangle.area, "Not work with same values!")
+
+    def test_perimeter_same_values(self):
+        tests = [(0, 0),
+                 (1, 4),
+                 (5, 20),
+                 (10**100, 4*10**100),
+                 (10**1000, 4*10**1000),
+                 (1.2, 4.8),
+                 (0.01, 0.04)]
+        self.__same_values_test(tests, rectangle.perimeter, "Not work with same values!")
